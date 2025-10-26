1b202e927708deeeb1e3f0ce8f898f7cdfa056bc
Asel <143624281+asel896@users.noreply.github.com>
2025-10-26 20:49:59 +0300

    Add SQL queries for film and customer data

---
+++ sorgularim.sql
@@ -0,0 +1,24 @@
+--1
+SELECT title,description
+* FROM film;
+
+--2
+SELECT * 
+FROM film 
+WHERE length>60 AND lenght<75;
+
+--3
+SELECT * 
+FROM film 
+WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+
+--4
+SELECT last_name 
+FROM customer 
+WHERE first_name = 'Mary';
+
+--5
+SELECT * 
+FROM film 
+WHERE lenght<=50 AND rental_rate NOT IN (2.99 ,4.99);
+
